8075eac7b6c9137df9677d514f0cfea8a701abae
javierpb275 <javierpb275@gmail.com>
2021-02-22 16:55:41 +0100

    created sevrice package and EmployeeService Interface.
     created EmployeeServiceImpl class that implements EmployeeService.
    
    EmployeeServiceImpl has methods with the @Transactional that call the methods from EmployeeDAO
    
    refactored code in EmployeeRestController to use EmployeeService

--- src/main/java/com/javi/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/javi/springboot/cruddemo/rest/EmployeeRestController.java
@@ -7,25 +7,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.javi.springboot.cruddemo.dao.EmployeeDAO;
 import com.javi.springboot.cruddemo.entity.Employee;
+import com.javi.springboot.cruddemo.service.EmployeeService;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
 	
-	private EmployeeDAO employeeDAO;
+	private EmployeeService employeeService;
 	
 	//inject employee dao (use constructor injection)
 	@Autowired
-	public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
-		employeeDAO = theEmployeeDAO;
+	public EmployeeRestController(EmployeeService theEmployeeService) {
+		employeeService = theEmployeeService;
 	}
 	
 	//expose endpoint "/employees" and return list of employees
 	@GetMapping("/employees")
 	public List<Employee> findAll() {
-		return employeeDAO.findAll();
+		return employeeService.findAll();
 	}
 
 }
